66a30eb2a58e347aaf24d98fd63d02b424edc555
Kymaeda <49601393+Kymaeda@users.noreply.github.com>
2022-10-20 07:27:00 +0900

    add task

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  const inputText = document.getElementById("input").value;
+  document.getElementById("input").value = "";
+
+  const li = document.createElement("li");
+
+  const div = document.createElement("div");
+  div.className = "list-row";
+  li.appendChild(div);
+  console.log(li);
+
+  const p = document.createElement("p");
+  p.className = "task-name";
+  p.innerHTML = inputText;
+  div.appendChild(p);
+
+  const completeBtn = document.createElement("button");
+  completeBtn.innerHTML = "完了";
+  const DeleteBtn = document.createElement("button");
+  DeleteBtn.innerHTML = "削除";
+  div.appendChild(completeBtn);
+  div.appendChild(DeleteBtn);
+
+  document.getElementById("incompletion-list").appendChild(li);
+};
+
+document.getElementById("add").addEventListener("click", () => onClickAdd());
+// document.getElementById("add").addEventListener("click", onClickAdd);
